f14e95785803fe850d42823f69acfffa4f0499d0
vaasu2906 <vasusinghal2906@gmail.com>
2025-11-24 13:07:24 +0530

    Shopping API

--- ReactWork/ShoppingCart/src/component/Dashboard.jsx
+++ ReactWork/ShoppingCart/src/component/Dashboard.jsx
@@ -1,20 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Dashboard() {
-  const [counter, setCounter] = useState(10);
-  const [pointer, setPointer] = useState(50);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log(counter);
-    console.log(pointer);
-  }, [counter]);
+    async function getData() {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const resdata = await response.json();
+      setData(resdata);
+      console.log(resdata);
+    }
+    getData();
+  }, []);
+  function AddtoCart(element){
+    alert(element.title)
+  }
 
   return (
-    <div>
-      <h2>Counter Value = {counter}</h2>
-      <h2>Pointer Value ={pointer}</h2>
-      <button onClick={() => setCounter(counter + 20)}>Increment Counter</button>
-      <button onClick={()=>setPointer(pointer+1)}>Increment Pointer</button>
+    <div >
+      {data.length === 0 ? (
+        <h2>Data not Found</h2>
+      ) : (
+        <div style={{display:"flex",justifyContent:"space-between",flexWrap:"wrap", marginTop:"40px"}}>
+          {/* Product Cards */}
+          {data.map((element) => (
+            <div
+              key={element.id}
+              style={{ border: "2px solid red", height: "500px", width: "30%", marginBottom: "20px",display: "flex",
+                flexDirection: "column",alignItems: "center",justifyContent: "center",boxSizing: "border-box",padding: "10px",}}
+            >
+              <img src={element.image} height={100} width={100} alt={element.title} />
+              <h3>{element.title}</h3>
+              <p>{element.description}</p> 
+              <p>{element.price}</p>
+              <button onClick={()=>AddtoCart(element)}>Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
